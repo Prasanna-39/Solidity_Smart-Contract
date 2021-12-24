e072b978d2b33b29652c54b9a28d1740046229c3
Prasanna-39 <74823833+Prasanna-39@users.noreply.github.com>
2021-12-24 13:26:33 +0530

    Create 6.function.sol

---
+++ 6.function.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract functionexample{
+
+    function addno(uint x,uint y)external pure returns(uint){
+        return x+y;
+
+    }
+    function subno(uint x,uint y)external pure returns(uint){
+        return x-y;
+    }
+}
