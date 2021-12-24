6e5da01fbdc7fb7c16644ceb3897d2b46099b953
Prasanna-39 <74823833+Prasanna-39@users.noreply.github.com>
2021-12-24 11:56:48 +0530

    Create 2.2.value-type.sol

---
+++ 2.2.value-type.sol
@@ -0,0 +1,19 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract valuetype{
+    int public myint=-123;// either positive or negative value 
+    uint public myuint =123;// positive value accepted in the uint
+    bool public mybool=true;// bool accept true or flase  
+    string public mystring="hello.prasanna";// accept only character or any value inside the ("") double quotation 
+    address public myadd=0x71C7656EC7ab88b098defB751B7401B5f6d8976F;
+    bytes32 public byte32=0x0000000000000000000000000000000000000000000000000000000001020304 ;
+
+
+    /// 
+
+
+
+
+}
