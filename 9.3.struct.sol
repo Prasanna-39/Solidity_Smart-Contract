046b01263c8da08d7a8051e92cd9808a11c71fd9
Prasanna-39 <74823833+Prasanna-39@users.noreply.github.com>
2021-12-28 15:03:56 +0530

    Create 9.3.struct.sol

---
+++ 9.3.struct.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+contract struct_{
+
+    struct Mystruct_ {
+        string name;
+        uint id;
+    }
+    Mystruct_ public mystruct = Mystruct_('prasanna',7);
+    
+
+
+struct MyStruct {
+        uint myUint;
+        string myString;
+    }
+
+    MyStruct public myStruct = MyStruct(1, "Hello, World!");
+
+
+}
